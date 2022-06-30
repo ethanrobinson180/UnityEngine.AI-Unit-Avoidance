dd95cfb13e0b2a851fab94e72dd8ff5cfce88608
Ethan Robinson <88520085+ethanrobinson180@users.noreply.github.com>
2022-06-30 00:04:23 -0500

    Add files via upload

---
+++ Avoidance.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine.AI;
+using UnityEngine;
+
+public class Avoidance : MonoBehaviour
+{
+    [Header("NavMesh Configurations")]
+    public int PathfindingIterationsPerFrame = 100;
+    public float AvoidancePredictionTime = 2;
+
+    private void Start()
+    {
+        NavMesh.pathfindingIterationsPerFrame = PathfindingIterationsPerFrame;
+        NavMesh.avoidancePredictionTime = AvoidancePredictionTime;
+    }
+}
